d5e168ba4b7c858957f0caed28bf4e9e2b8d8abd
amowy <gyorgy.matyi@gmail.com>
2024-03-09 15:01:02 +0200

    convention ficsed

--- dblib/tables.py
+++ dblib/tables.py
@@ -8,7 +8,7 @@ mydb = myclient["matyi_test"]
 create_table_regex = re.compile(r'Create Table (\w+)', re.IGNORECASE)
 drop_table_regex = re.compile(r'Drop Table (\w+)', re.IGNORECASE)
 
-def createTable(name: str) -> None:
+def create_table(name: str) -> None:
     if name in mydb.list_collection_names():
         print("table with given name allready exists in the database")
         return
@@ -16,7 +16,7 @@ def createTable(name: str) -> None:
     mycol.insert_one({'init': 1})
     print("table created successfully")
         
-def dropTable(name: str) -> None:
+def drop_table(name: str) -> None:
     if name not in mydb.list_collection_names():
         print("table with given name does not exist in the database")
         return
@@ -24,7 +24,7 @@ def dropTable(name: str) -> None:
     mycol.drop()
     print("table deleted successfully")
     
-def listTables() -> None:
+def list_tables() -> None:
     print(mydb.list_collection_names())
     
     
@@ -45,10 +45,10 @@ while True:
     command = read_command()  
     if create_table_regex.match(command):
         valami = create_table_regex.search(command)
-        createTable(valami.group(1))
+        create_table(valami.group(1))
     elif drop_table_regex.match(command):
         valami = drop_table_regex.search(command)
-        dropTable(valami.group(1))
+        drop_table(valami.group(1))
     else:
         print("Command not known")
         
