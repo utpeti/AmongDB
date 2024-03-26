f06883af33d92d10fa7e4ac754e6411fe095f16b
amowy <gyorgy.matyi@gmail.com>
2024-03-26 04:36:58 +0200

    idkmiezdebocsi

--- dblib/lib.py
+++ dblib/lib.py
@@ -8,10 +8,11 @@ TYPES = ['INT', 'FLOAT', 'BIT', 'DATE', 'DATETIME', 'VARCHAR']
 
 ### TABLE FUNCTIONS: ###
 
-def create_table(name: str, content: str) -> None:
+def create_table(name: str, content: str) -> str:
     if name in mydb.list_collection_names():
-        print("A TABLE NAMED " + name + " ALREADY EXISTS IN THE DATABASE!")
-        return
+        msg = "A TABLE NAMED " + name + " ALREADY EXISTS IN THE DATABASE!"
+        print(msg)
+        return msg
     mycol = mydb[name]
     # CREATING DICT FROM CONTENT
     tablestruct = {}
@@ -20,39 +21,63 @@ def create_table(name: str, content: str) -> None:
         if line[1] in TYPES:
             tablestruct[line[0]] = line[1]
     mycol.insert_one(tablestruct)
-    print("TABLE " + name + " CREATED!")
+    msg = "TABLE " + name + " CREATED!"
+    print(msg)
+    return msg
         
-def drop_table(name: str) -> None:
+def drop_table(name: str) -> str:
     if name not in mydb.list_collection_names():
-        print("A TABLE NAMED " + name + " DOES NOT EXIST IN THE DATABASE!")
-        return
+        msg = "A TABLE NAMED " + name + " DOES NOT EXIST IN THE DATABASE!"
+        print(msg)
+        return msg
     mycol = mydb[name]
     mycol.drop()
-    print("TABLE " + name + " DROPPED!")
+    msg = "TABLE " + name + " DROPPED!"
+    print(msg)
+    return msg
     
-def list_tables() -> None:
-    print(mydb.list_collection_names())
+def list_tables():
+    return mydb.list_collection_names()
 
 ### DATABASE FUNCTIONS: ###
 
-def create_database(databaseName: str) -> None:
+def create_database(databaseName: str) -> str:
     dbnames = cluster.list_database_names()
     if databaseName in dbnames:
-        print("A DATABASE NAMED " + databaseName + " ALREADY EXISTS!")
+        msg = "A DATABASE NAMED " + databaseName + " ALREADY EXISTS!"
     else:
         db = cluster[databaseName]
         initcollection = db[databaseName]
         initcollection.insert_one({'DATABASE CREATED': True})
-        print("DATABASE " + databaseName + " CREATED!")
+        msg = "DATABASE " + databaseName + " CREATED!"
         #db.drop_collection(initcollection) emiatt letrejon de torlodik is a database
+        #talan hasonlo modon meg lehetne oldani tablenel(?)
+    print(msg)
+    return msg
 
-def drop_database(databaseName: str) -> None:
+def drop_database(databaseName: str) -> str:
     dbnames = cluster.list_database_names()
     if databaseName in dbnames:
         cluster.drop_database(databaseName)
-        print("DATABASE " + databaseName + " DROPPED!")
+        msg = "DATABASE " + databaseName + " DROPPED!"
     else:
-        print("DATABASE " + databaseName + " DOES NOT EXIST!")
+        msg = "DATABASE " + databaseName + " DOES NOT EXIST!"
+    print(msg)
+    return msg
+        
+def list_databases():
+    db_list = cluster.list_database_names()
+    try:
+        db_list.remove('admin')
+        db_list.remove('local')
+    except:
+        pass
+    return db_list
+
+def select_curr_database(name: str) -> None:
+    global mydb
+    mydb = cluster[name]
+    
 
 ### INDEX FUNCTIONS: ###
 
@@ -67,7 +92,7 @@ def create_index(indexName: str, tableName: str, columns: str) -> None:
                 return
         
         for column in columns:
-            collection.create_index(column, name=str(indexName))
+            collection.create_index(column, name=str(indexName)) #maybe nem kell castelni, de igy megy so igy hagyom aztan ott rohad meg valoszinu
             
         print(f"INDEX '{indexName}' CREATED ON TABLE '{tableName}' FOR COLUMNS: {columns}")
     else:
---
+++ dblib/server2.py
@@ -0,0 +1,60 @@
+from flask import Flask, request, jsonify
+
+import lib
+import re
+
+create_database_regex = re.compile(r'Create Database (\w+)', re.IGNORECASE)
+drop_database_regex = re.compile(r'Drop Database (\w+)', re.IGNORECASE)
+create_table_regex = re.compile(r'Create Table (\w+)', re.IGNORECASE)
+drop_table_regex = re.compile(r'Drop Table (\w+)', re.IGNORECASE)
+create_index_regex = re.compile(r'Create Index (\w+)', re.IGNORECASE)
+
+app = Flask(__name__,
+            static_url_path="", 
+            static_folder='static')
+
+
+@app.route("/api/database/commands", methods=['POST'])
+def get_databases():
+    valami = request.json
+    return sch(valami['text'])
+
+def sch(command: str):
+    commandMsg = "Command not known"
+    if create_database_regex.match(command):
+        valami = create_database_regex.search(command)
+        commandMsg = lib.create_database(valami.group(1))
+    elif drop_database_regex.match(command):
+        valami = drop_database_regex.search(command)
+        commandMsg = lib.drop_database(valami.group(1))
+    elif create_table_regex.match(command):
+        valami = create_table_regex.search(command)
+        commandMsg = lib.create_table(valami.group(1),command)
+    elif drop_table_regex.match(command):
+        valami = drop_table_regex.search(command)
+        commandMsg = lib.drop_table(valami.group(1))
+    elif create_index_regex.match(command):
+        match = create_index_regex.search(command)
+        index_name = match.group(1)
+        table_name = match.group(2)
+        columns = match.group(3).split(', ')
+        lib.create_index(index_name, table_name, columns)
+    print(commandMsg)
+    return commandMsg
+        
+@app.route("/api/database/db_list", methods=['GET'])
+def send_databases():
+    databases = lib.list_databases()
+    return jsonify(databases)
+
+@app.route("/api/database/select_db", methods=['POST']) 
+def select_curr_database():
+    name = request.json
+    lib.select_curr_database(name['curr_db'])
+    return ""
+
+
+@app.route("/api/table/table_list", methods=['GET'])
+def send_tables():
+    tables = lib.list_tables()
+    return jsonify(tables)
